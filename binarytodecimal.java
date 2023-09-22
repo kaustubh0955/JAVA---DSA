243260a8490fdca2c37716e307df0ea544a124e2
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-09-22 23:00:13 +0530

    day2

---
+++ binarytodecimal.java
@@ -0,0 +1,33 @@
+package Functions;
+import java.util.*;
+public class binarytodecimal {
+  public static void binToDec(int binNum){
+  int pow=0;
+  int decNum=0;
+
+  while(binNum>0){
+  int lastDigit=binNum % 10;
+  decNum=(int) (decNum+(lastDigit*Math.pow(2,pow)));
+  }
+  System.out.println("decimal of" +binNum+"="+decNum);
+  }
+
+  public static void dectovbin(int n){
+  int myNum=n;
+  int pow=0;
+  int binNum=0;
+
+  while(n>0){
+  int rem=n%2;
+  binNum=binNum+(rem*(int)Math.pow(10,pow));
+
+  pow++;
+  n=n/2;
+  }
+  System.out.println("binary form of"+myNum+"="+binNum);
+  }
+  public static void main(String args[]){
+  binToDec(101);
+  dectovbin(7);
+  }
+}
---
+++ prime.java
@@ -0,0 +1,30 @@
+package Functions;
+//Important
+import java.util.*;
+public class prime {
+  /*public static boolean isPrime(int n){
+  boolean isPrime=true;
+  for(int i=2;i<=n-1;i++){
+  if(n%i==0){
+  isPrime=false;
+  break;
+  //return isPrime
+
+  }
+  }
+  return isPrime;
+  }*/
+  //Optimized solution
+  public static boolean isPrime(int n){
+  if(n==2) return true;
+  for(int i=2;i<Math.sqrt(n);i++){
+  if(n%i==0){
+  return false;
+  }
+  }
+  return true;
+  }
+  public static void main(String args[]){
+  System.out.println(isPrime(16));
+  }
+}
