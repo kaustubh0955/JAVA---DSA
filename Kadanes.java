137929d4787da2ad82ff87770cedf323093e845f
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-09-24 00:14:47 +0530

    complete

--- Kadanes.java
+++ Kadanes.java
@@ -2,35 +2,7 @@ import java.util.*;
 
 public class Kadanes
 {
-    public static void maxxsubarray(int numbers[]){
-        
-        int currSum=0;
-        int maxSum=Integer.MIN_VALUE;
-        int prefix[]=new int[numbers.length];
-        
-        prefix[0]=numbers[0];
-        for(int i=0;i<prefix.length;i++){
-            prefix[i]=prefix[i-1]+numbers[i];
-        }
-        for(int i=0;i<numbers.length;i++){
-            int start=i;
-            for(int j=i;j<numbers.length;j++){
-                int end=j;
-                currSum= start==0 ? prefix[end]:prefix[end]-prefix[start-1];
-             
-                
-                 if(maxSum<currSum){
-                     maxSum=currSum;
-                     
-                 }
-            
-             }
-             
-         }
-         System.out.println("max sum="+maxSum);
-        
-        
-    }
+   
     
     public static void kadanes(int numbers[]){
         int ms=Integer.MIN_VALUE;
