deaf72546f6cfa6bd24b3786a1332c6ac590c208
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-09-29 00:11:08 +0530

    day7

---
+++ countsetbit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class countsetbit {
+public static int countset(int n){
+int count =0;
+while(n>0){
+if((n&1)!=0){
+count++;
+}
+n=n>>1;
+}
+return count;
+}
+  public static void main(String args[]){
+  System.out.println(countset(10));
+  }
+}
