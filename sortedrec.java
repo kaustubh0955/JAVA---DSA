030843ae1c9cb2b3ba0372d7912692fc7e4c5074
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-01 01:39:46 +0530

    day2q2

---
+++ sortedrec.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class sortedrec {
+  
+  public static boolean isSorted(int arr[], int i) {
+    if (i == arr.length - 1) { // Check if i is at the second-to-last index
+      return true;
+    }
+    
+    if (arr[i] <= arr[i + 1]) {
+      return isSorted(arr, i + 1);
+    }
+    
+    return false;
+  }
+  
+  public static void main(String args[]) {
+    int arr[] = {1, 2, 3, 4, 5};
+    System.out.println(isSorted(arr, 0));
+  }
+}
