ced5951c177fe97ba15afb23c46fb8387c77c643
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-01 22:55:30 +0530

    dayy2

---
+++ reverse1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public String reverseWords(String s) {
+       
+        String[] words = s.split(" ");
+        
+      
+        StringBuilder reversed = new StringBuilder();
+        for (String word : words) {
+            StringBuilder wordBuilder = new StringBuilder(word);
+            wordBuilder.reverse();
+            reversed.append(wordBuilder).append(" ");
+        }
+        
+        
+        if (reversed.length() > 0) {
+            reversed.setLength(reversed.length() - 1);
+        }
+        
+        return reversed.toString();
+    }
+
+    public static void main(String args[]) {
+        String s = "God Ding";
+        Solution solution = new Solution();
+        System.out.println(solution.reverseWords(s));
+    }
+}
