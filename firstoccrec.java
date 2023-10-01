8974e99994a342c91cb426fb5afd8b1fd6025fe9
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-01 12:00:37 +0530

    day2

---
+++ firstoccrec.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class firstoccrec {
+  
+  public static int firstoccur(int arr[],int key,int i){
+  if(i==arr.length){
+  return -1;
+  
+  }
+  
+  if(arr[i]==key){
+  return i;
+  }
+  return firstoccur(arr, key, i+1);
+  }
+  public static void main(String args[]){
+  int arr[]={8,3,6,9,5,10,2,5,3};
+  System.out.println(firstoccur(arr, 5, 0));
+  }
+}
