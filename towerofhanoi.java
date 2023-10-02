79170835f4312ee94772ffab082de9fc98a7cec3
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-02 23:16:10 +0530

    day3q15

---
+++ towerofhanoi.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class towerofhanoi {
+  
+  public static void toh(int n,String first,String second,String third){
+  if(n==1){
+  System.out.println("Transfer of disk " +n+ "from" +first+ "to" +third);
+  return;
+  }
+
+  toh(n-1, first, third, second);
+
+  System.out.println("Transfer of disk "+n +"from" +first+ "to" +second);
+
+  toh(n-1, second, first, third);
+  }
+
+  public static void main(String args[]){
+  int n=4;
+  toh(n, "A", "B", "C");
+  }
+}
