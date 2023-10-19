5d7468a723868c5626061169a78cd14e13dd8b46
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-10-19 15:30:12 +0530

    backtrack

---
+++ backtrackingarr.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class backtrackingarr {
+
+public static void changearr(int arr[],int i,int val){
+//base case
+if(i==arr.length){
+printArr(arr);
+return;
+}
+//recursion
+arr[i]=val;
+changearr(arr, i+1, val+1);
+arr[i]=arr[i]-2;
+}
+public static void printArr(int arr[]){
+for(int i=0;i<arr.length;i++){
+System.out.print(arr[i]+" ");
+}
+System.out.println();
+}
+  public static void main(String args[]){
+  int arr[]=new int[5];
+changearr(arr, 0, 1);
+printArr(arr);
+  }
+}
