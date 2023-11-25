92e51eece08388d4ddaafcd6a10f734cb357eded
kaustubh0955 <kaustubhrai0955@gmail.com>
2023-11-25 23:30:03 +0530

    backtrack

--- backtrackingarr.java
+++ backtrackingarr.java
@@ -14,7 +14,7 @@ arr[i]=arr[i]-2;
 }
 public static void printArr(int arr[]){
 for(int i=0;i<arr.length;i++){
-System.out.print(arr[i]+" ");
+System.out.print(arr[i] +" ");
 }
 System.out.println();
 }
